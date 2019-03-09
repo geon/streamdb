f55ed92659ac9a71dd5dbeb479654f5ce1de764c
Victor Widell <victor@topmost.se>
2019-03-09 12:40:05 +0100

    Added example.

---
+++ src/example2.ts
@@ -0,0 +1,37 @@
+import { StreamDb } from "./streamdb";
+import { makeAsyncGeneratorAdapter } from "./makeAsyncGeneratorAdapter";
+
+const asyncGenerator = makeAsyncGeneratorAdapter<number>(
+	async asyncTerminator => {
+		for (;;) {
+			await asyncTerminator.next(1);
+			await new Promise(resolve => setTimeout(resolve, 200));
+		}
+	},
+);
+
+const db = new StreamDb(
+	asyncGenerator,
+	0,
+	async (state, _event, streams) => {
+		const counter = state + 1;
+
+		await streams.counter.next(counter);
+
+		return counter;
+	},
+	{ counter: undefined },
+	() => {},
+);
+
+(async () => {
+	for await (const counter of db.getStream("counter")) {
+		console.log("From beginning", counter);
+	}
+})();
+
+setTimeout(async () => {
+	for await (const counter of db.getStream("counter")) {
+		console.log("Subscribed later", counter);
+	}
+}, 2000);
